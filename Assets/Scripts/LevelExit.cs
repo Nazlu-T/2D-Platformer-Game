cb3623db6b4ae23459d85abdc065459822c70ef5
Nazlı Tekin <nazliecetekin@gmail.com>
2025-01-10 14:40:12 +0300

    Update LevelExit.cs

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -9,7 +9,11 @@ public class LevelExit : MonoBehaviour
     [SerializeField] float  LevelLoadDelay = 1f;
      void OnTriggerEnter2D(Collider2D other) 
     {
-       StartCoroutine(LoadNextLevel());
+        if (other.tag == "Player")
+        {
+             StartCoroutine(LoadNextLevel());
+        }
+      
     }
 
      IEnumerator LoadNextLevel()
