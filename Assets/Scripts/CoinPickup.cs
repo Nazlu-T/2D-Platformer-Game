9f8f289c63d12077c1c9eb81d8fcf4f4f7238815
Nazlı Tekin <nazliecetekin@gmail.com>
2025-01-14 01:07:07 +0300

    UI added

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinSfx;
    void OnTriggerEnter2D(Collider2D other)
    {
     if(other.tag == "Player")
     {
-        
+        AudioSource.PlayClipAtPoint(coinSfx, Camera.main.transform.position);
         Destroy(gameObject);
 
     }
+    
+    
 
    }
 }
